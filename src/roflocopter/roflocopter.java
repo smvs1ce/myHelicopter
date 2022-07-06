ca2b182228bafaad172730fbb8421a6924bb5f2e
John Hairston <j.hairston71@gmail.com>
2022-07-06 10:39:08 -0400

    my helicopter

---
+++ src/roflocopter/roflocopter.java
@@ -0,0 +1,16 @@
+package roflocopter;
+
+public class roflocopter {
+
+	public static void main(String[] args) {
+		System.out.println("ROFL:ROFL:LOL:ROFL:ROFL:ROFL");
+		System.out.println("           A                  ");
+		System.out.println("  L   /-----------               ");
+		System.out.println(" LOL====        []\\          ");
+		System.out.println("  L    \\           \\");
+		System.out.println("        \\___________]               ");
+		System.out.println("           I       I          ");
+		System.out.println("        -------------/         ");
+		System.out.println("      John's Helicopter!       ");
+  }
+}
